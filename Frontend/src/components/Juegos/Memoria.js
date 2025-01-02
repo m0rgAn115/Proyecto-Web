a4d80fa36aa82c681e4ded27376e0375b9989eec
Angel Mondragon <damianmor3005@hotmail.com>
2025-01-01 21:05:25 -0600

    Finalizacion de detalles del juego

--- Frontend/src/components/Juegos/Memoria.js
+++ Frontend/src/components/Juegos/Memoria.js
@@ -4,21 +4,65 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import * as tf from "@tensorflow/tfjs"
 import Tarjeta from "./Memoria-Comps/tarjeta";
+import { getRandomInteger } from "@tensorflow-models/speech-commands/dist/generic_utils";
+import { Redirect } from "react-router-dom/cjs/react-router-dom";
 // const speech = speechCommands.create('BROWSER_FFT');
 // import * as speech from "@tensorflow-models/speech-commands"
 
 const listaColores = [
   "bg-green-500",
-  "bg-red-500",
-  "bg-pink-500",
-  "bg-blue-500",
-  "bg-orange-500",
-  "bg-lime-500",
-  "bg-cyan-500",
-  "bg-emerald-500",
-  "bg-violet-500",
-  "bg-fuchsia-500",
+"bg-red-500",
+"bg-pink-500",
+"bg-blue-500",
+"bg-orange-500",
+"bg-lime-500",
+"bg-cyan-500",
+"bg-emerald-500",
+"bg-violet-500",
+"bg-fuchsia-500",
+"bg-yellow-500",
+"bg-teal-500",
+"bg-rose-500",
+"bg-indigo-500",
+"bg-purple-500",
+"bg-amber-500",
+"bg-gray-500",
+"bg-sky-500",
+"bg-lime-700",
+"bg-emerald-700",
+
 ]
+
+const listaColores_texto = [
+  "text-green-500",
+  "text-red-500",
+  "text-pink-500",
+  "text-blue-500",
+  "text-orange-500",
+  "text-lime-500",
+  "text-cyan-500",
+  "text-emerald-500",
+  "text-violet-500",
+  "text-fuchsia-500",
+  "text-yellow-500",
+  "text-teal-500",
+  "text-rose-500",
+  "text-indigo-500",
+  "text-purple-500",
+  "text-amber-500",
+  "text-gray-500",
+  "text-sky-500",
+  "text-lime-700",
+  "text-emerald-700",
+];
+
+
+const puntos_por_ronda = {
+  "facil": 100,
+  "medio": 200,
+  "dificil": 400,
+  "extremo": 700
+}
 
 class Memoria extends Component {
   state = {
@@ -28,7 +72,7 @@ class Memoria extends Component {
     dificultad: "facil",
     rondaActual: 0,
     secuencia: [],
-    cantidadRondas: 3,
+    cantidadRondas: 15,
     indiceActual: 0,
     comandoActual: null,
     colorActual: undefined,
@@ -36,7 +80,8 @@ class Memoria extends Component {
     jugadorRespondiendo: false,
     comandosUsuario: [],
     comandoActualUsuario: null,
-    esCorrecta: undefined
+    esCorrecta: undefined,
+    puntuacion: 0
   };
 
   componentDidMount() {
@@ -108,6 +153,8 @@ class Memoria extends Component {
 
     const { dificultad, labels,rondaActual  } = this.state;
 
+   
+
     switch (dificultad) {
       case "facil": {
         const instrucciones = [labels[2], labels[7], labels[11], labels[17]];
@@ -116,8 +163,9 @@ class Memoria extends Component {
         let nuevosColores = []
         for (let j = 0; j < 3 + parseInt(rondaActual / 2); j++) {
           const randomInt = Math.floor(Math.random() * instrucciones.length);
+          const randomIntColor = Math.floor(Math.random() * listaColores.length);
           nuevaSecuencia.push(instrucciones[randomInt]);
-          nuevosColores.push(listaColores[randomInt])
+          nuevosColores.push(listaColores[randomIntColor])
         }
 
         
@@ -132,6 +180,104 @@ class Memoria extends Component {
             // Este callback se ejecuta después de que el estado se ha actualizado
             console.log(`Secuencia completa: ${this.state.secuencia}`);
             this.intervalo = setInterval(this.imprimirValor, 2000);
+            setTimeout(this.validarRespuesta,5000)
+          }
+        );
+
+        
+      }
+      break;
+
+      case "medio": {
+        const instrucciones = [labels[2], labels[7], labels[11], labels[17], labels[6], labels[14], labels[9], labels[18]];
+
+        let nuevaSecuencia = []; // Variable temporal para construir la secuencia
+        let nuevosColores = []
+        for (let j = 0; j < 3 + parseInt(rondaActual / 2); j++) {
+          const randomInt = Math.floor(Math.random() * instrucciones.length);
+          const randomIntColor = Math.floor(Math.random() * listaColores.length);
+          nuevaSecuencia.push(instrucciones[randomInt]);
+          nuevosColores.push(listaColores[randomIntColor])
+        }
+
+        
+        console.log("Secuencia ronda: ",rondaActual,nuevaSecuencia);
+
+        this.setState(
+          {
+            secuencia: nuevaSecuencia,
+            coloresTarjetas: nuevosColores
+          },
+          () => {
+            // Este callback se ejecuta después de que el estado se ha actualizado
+            console.log(`Secuencia completa: ${this.state.secuencia}`);
+            this.intervalo = setInterval(this.imprimirValor, 2000);
+            setTimeout(this.validarRespuesta,5000)
+          }
+        );
+
+        
+      }
+      break;
+
+      case "dificil": {
+        const instrucciones = [labels[2], labels[7], labels[11], labels[17], labels[6], labels[14], labels[9], labels[18], labels[19],  labels[10],  labels[16]];
+
+        let nuevaSecuencia = []; // Variable temporal para construir la secuencia
+        let nuevosColores = []
+        for (let j = 0; j < 4 + parseInt(rondaActual / 2); j++) {
+          const randomInt = Math.floor(Math.random() * instrucciones.length);
+          const randomIntColor = Math.floor(Math.random() * listaColores.length);
+          nuevaSecuencia.push(instrucciones[randomInt]);
+          nuevosColores.push(listaColores[randomIntColor])
+        }
+
+        
+        console.log("Secuencia ronda: ",rondaActual,nuevaSecuencia);
+
+        this.setState(
+          {
+            secuencia: nuevaSecuencia,
+            coloresTarjetas: nuevosColores
+          },
+          () => {
+            // Este callback se ejecuta después de que el estado se ha actualizado
+            console.log(`Secuencia completa: ${this.state.secuencia}`);
+            this.intervalo = setInterval(this.imprimirValor, 1000);
+            setTimeout(this.validarRespuesta,5000)
+          }
+        );
+
+        
+      }
+      break;
+
+      case "extremo": {
+        const instrucciones = labels.slice(2,19);
+        console.log("longitud en extemo: ", instrucciones.length);
+        
+
+        let nuevaSecuencia = []; // Variable temporal para construir la secuencia
+        let nuevosColores = []
+        for (let j = 0; j < 5 + parseInt(rondaActual / 2); j++) {
+          const randomInt = Math.floor(Math.random() * instrucciones.length);
+          const randomIntColor = Math.floor(Math.random() * listaColores.length);
+          nuevaSecuencia.push(instrucciones[randomInt]);
+          nuevosColores.push(listaColores[randomIntColor])
+        }
+
+        
+        console.log("Secuencia ronda: ",rondaActual,nuevaSecuencia);
+
+        this.setState(
+          {
+            secuencia: nuevaSecuencia,
+            coloresTarjetas: nuevosColores
+          },
+          () => {
+            // Este callback se ejecuta después de que el estado se ha actualizado
+            console.log(`Secuencia completa: ${this.state.secuencia}`);
+            this.intervalo = setInterval(this.imprimirValor, 500);
             setTimeout(this.validarRespuesta,5000)
           }
         );
@@ -200,8 +346,24 @@ class Memoria extends Component {
     })
   }
 
+  terminarJuego = () => {
+    const { rondaActual, cantidadRondas, dificultad } = this.state;
+
+    const dificultad_formateada = dificultad.toLowerCase()
+
+    const puntos_ronda = puntos_por_ronda[dificultad_formateada]
+
+    return <Redirect to={`/`} />;
+  }
+
   iniciarRonda = () => {
-    const { rondaActual, cantidadRondas } = this.state;
+
+    const { rondaActual, cantidadRondas, dificultad } = this.state;
+
+    const dificultad_formateada = dificultad.toLowerCase()
+
+    const puntos_ronda = puntos_por_ronda[dificultad_formateada]
+
     this.limpiarValores()
     if (rondaActual < cantidadRondas) {
       this.setState(
@@ -212,6 +374,7 @@ class Memoria extends Component {
         () => {
           console.log("Nueva ronda:", this.state.rondaActual);
           this.generarSecuencia();
+          this.state.puntuacion = puntos_ronda*rondaActual
         }
       );
     } else {
@@ -222,28 +385,26 @@ class Memoria extends Component {
 
   render() {
     const {
-      labels,
-      comandoActual,
-      colorActual,
       rondaActual,
-      jugadorRespondiendo,
-      comandoActualUsuario,
       comandosUsuario,
-      secuencia,
-      esCorrecta,
-      dificultad
+      puntuacion
     } = this.state;
 
     return (
       <div className="flex flex-col h-screen">
         {/* Barra superior */}
         <div className="flex flex-row justify-between px-5 items-center bg-blue-800 text-white h-10 leading-none">
-          <button className="m-0 font-bold hover:scale-[1.03]">Regresar</button>
-          <p className="text-md m-0 font-bold">Juego de Memoria!</p>
+          <button className="m-0 font-bold hover:scale-[1.03] z-30">Regresar</button>
+          <div>
+            <p className="text-md m-0 font-bold">Juego de Memoria!</p>
+            <p className="text-md m-0 font-bold text-md">Puntuacion: {puntuacion}</p>
+          </div>
+
           <div>
             <p className="text-sm m-0">Modo: {this.state.dificultad}</p>
             <p className="text-sm m-0">Ronda: {this.state.rondaActual}</p>
           </div>
+          
         </div>
     
         {/* Área del juego */}
@@ -267,10 +428,7 @@ class Memoria extends Component {
                 <div className="mx-auto flex flex-row justify-center mt-10">
                   {comandosUsuario.map((c, index) => (
                     <p
-                      className={`mx-10 font-bold text-xl text-${listaColores[index].slice(
-                        3,
-                        listaColores[index].length
-                      )}`}
+                      className={`mx-1 font-bold text-xl ${listaColores_texto[index]}`}
                     >
                       {` ${c} `}
                     </p>
@@ -283,9 +441,9 @@ class Memoria extends Component {
         </div>
     
         {/* Indicador de resultado */}
-        {this.state.esCorrecta !== undefined || true && (
-          <div className="absolute inset-0 flex flex-col items-center justify-center bg-gray-100 bg-opacity-40 rounded-lg h-1/2 w-1/2 mx-auto my-auto">
-            {this.state.esCorrecta || true ? (
+        {this.state.esCorrecta !== undefined  && (
+          <div className="absolute inset-0 flex flex-col items-center justify-center bg-white rounded-lg h-full w-full mx-auto my-auto">
+            {this.state.esCorrecta ? (
               <>
               <div className="flex flex-col items-center justify-center w-32 h-32 bg-green-500 rounded-full animate-fade-in">
                 <svg
@@ -301,9 +459,18 @@ class Memoria extends Component {
                 {/* <p className="mt-5 text-black text-lg font-medium">¡Correcto!</p> */}
               </div>
               <p className="text-lg" >Correcto!!</p>
+
+              <button
+                className="bg-blue-500 text-white px-10 py-3 mt-5 rounded-lg hover:bg-blue-600 transition"
+                onClick={this.iniciarRonda}
+              >
+                Siguiente ronda!
+              </button>
               </>
 
+
             ) : (
+              <>
               <div className="flex flex-col items-center justify-center w-32 h-32 bg-red-500 rounded-full animate-fade-in">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -315,15 +482,18 @@ class Memoria extends Component {
                 >
                   <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
-                <p className="mt-5 text-black text-lg font-medium">¡Incorrecto!</p>
               </div>
-            )}
-            <button
+              <p className="mt-5 text-black text-lg font-medium">¡Incorrecto!</p>
+
+              <button
               className="bg-blue-500 text-white px-10 py-3 mt-5 rounded-lg hover:bg-blue-600 transition"
-              onClick={this.iniciarRonda}
-            >
-              {this.state.esCorrecta ? "Siguiente ronda" : "Intentar de nuevo"}
+              onClick={this.terminarJuego}
+              >
+              Terminar Partida
             </button>
+              </>
+            )}
+            
           </div>
         )}
     
@@ -341,9 +511,10 @@ class Memoria extends Component {
                 value={this.state.dificultad}
                 className="mx-2 px-3 py-1 border rounded "
               >
-                <option value="Facil">Fácil</option>
-                <option value="Medio">Medio</option>
-                <option value="Dificil">Difícil</option>
+                <option value="facil">Fácil</option>
+                <option value="medio">Medio</option>
+                <option value="dificil">Difícil</option>
+                <option value="extremo">Extremo</option>
               </select>
             </div>
             
